6fac72e801975d62ec11fd2b552fb9d782f18fc8
leovartoomian <leovartoomian@gmail.com>
2021-07-11 10:24:05 +0430

    set ejs main configs

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,15 @@ const express=require('express');
 
 const app=express();
 
-console.log(express.static(path.join(__dirname,"../frontend")));
+app.use(express.static(path.join(__dirname,"../frontend")));
+
+app.set("view engine","ejs");
+app.set("views","../frontend/views")
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.listen(3000,()=>{
+    console.log('Server is running on Port 3000')
+})
