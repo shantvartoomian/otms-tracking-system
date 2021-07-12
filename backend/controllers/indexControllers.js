1ac86030f9af6f7fdbc38c94025a67bdd110c2cf
leovartoomian <leovartoomian@gmail.com>
2021-07-12 16:26:24 +0430

    change

--- backend/controllers/indexControllers.js
+++ backend/controllers/indexControllers.js
@@ -1,9 +1,13 @@
-const { QueryTypes } = require('sequelize');
+const {QueryTypes} = require('sequelize');
+
+
+const sequelize=require('../utils/database');
+
 
 module.exports.showIndex=async(req,res)=>{
     res.render("index",{
         pageTitle:'Track & Trace',
-        BlNumber:null
+        result:null
     })
 }
 
@@ -14,11 +18,38 @@ module.exports.showProfile=async(req,res)=>{
 }
 
 module.exports.getData=async(req,res)=>{
-    // if(req.body.filterText){
-    //     const result=await sequelize.query('SE')
-    // }
-    res.render("index",{
-        pageTitle:'Track & Trace',
-        BlNumber:req.body.BlNumber
-    })
+    var result=new Array()
+ 
+        try {
+            if(req.body.BlNumber){
+                data=await sequelize.query
+                ('SELECT        TOP (100) PERCENT dbo.TbBL.PNo, dbo.TbBL.FileNo, dbo.TbBL.HBL, dbo.TbBL.BLIssueDateH, ISNULL(dbo.TbVoyage.ETA, TbVoyage_1.ETA) AS ETA, ISNULL(dbo.TbVoyage.ArrivalDate, dbo.TbBLCAT.DischargeDate) '
+                +'AS ArrivalDate, dbo.TbBL.DORcvdDate, dbo.TbBL.DOSendDate, dbo.TbCNTRBase.CNTR AS Sea_CNTR, dbo.TbCNTR.GateOut AS Sea_GateOut, dbo.TbCNTR.MTRTN AS Sea_MTRTN, dbo.TbTruck.TruckNo AS OverLand_TruckNo, '
+                +'dbo.TbTruck.DispatchDate AS Overland_Dispatch, dbo.TbTruck.BorderCrossDate AS Overland_BorderCrossing, dbo.TbBLCAT.DischargeDate AS Overland_Discharge, dbo.TbBL.TransportationMode '
+                +'FROM            dbo.TbTruck RIGHT OUTER JOIN '
+                +'dbo.TbBLCAT RIGHT OUTER JOIN '
+                +'dbo.TbBL LEFT OUTER JOIN '
+                +' dbo.TbVoyage AS TbVoyage_1 ON dbo.TbBL.FirstVID = TbVoyage_1.ID ON dbo.TbBLCAT.TransportationMode = dbo.TbBL.TransportationMode AND dbo.TbBLCAT.BLID = dbo.TbBL.ID ON dbo.TbTruck.ID = dbo.TbBLCAT.CATID AND '
+                +' dbo.TbTruck.TransportationMode = dbo.TbBLCAT.TransportationMode LEFT OUTER JOIN'
+                +' dbo.TbCNTR ON dbo.TbBLCAT.TransportationMode = dbo.TbCNTR.TransportationMode AND dbo.TbBLCAT.CATID = dbo.TbCNTR.ID LEFT OUTER JOIN'
+                +' dbo.TbCNTRBase ON dbo.TbCNTR.CNTRBaseID = dbo.TbCNTRBase.ID LEFT OUTER JOIN'
+                +' dbo.TbVoyage ON dbo.TbBL.VID = dbo.TbVoyage.ID'
+                +' WHERE        (dbo.TbBL.RecStatus > 0) AND (dbo.TbBL.HBL = :HBL)',{
+                    replacements:{HBL:req.body.BlNumber},
+                    type:QueryTypes.SELECT
+                })
+                if(data.length>0){
+
+                    result.push(data)
+                }
+            }
+    
+             res.render("index",{
+                pageTitle:'Track & Trace',
+                result : result.length>0 ? result[0] : null
+            })
+        } catch (err) {
+            console.log(err)          
+        }
+
 }
