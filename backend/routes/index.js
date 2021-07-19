753dc9879631a5b92f797b65bf42fdc7415cde1d
leovartoomian <leovartoomian@gmail.com>
2021-07-19 12:32:24 +0430

    change

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,14 @@
 const express=require('express');
 
-const {getData,showIndex,showProfile,showSignUp}=require('../controllers/indexControllers');
+const {getData,showIndex,showProfile,showSignIn,login}=require('../controllers/indexControllers');
 
 const router=express.Router();
 
 
 router.get("/",showIndex)
 router.get("/profile",showProfile)
-router.get("/signup",showSignUp)
+router.get("/signIn",showSignIn)
 router.post("/sendData",getData)
+router.post("/login",login)
 
 module.exports=router;
