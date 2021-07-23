0fd701ff80226c7c7db9db845acfab78b890fd5b
leovartoomian <leovartoomian@gmail.com>
2021-07-23 17:45:32 +0430

    changes

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -7,17 +7,7 @@ const sequelize=require('../utils/database');
 
 
 module.exports.printArrivalNotice=async(req,res)=>{
-
-    
-    Stimulsoft.Base.StiLicense.loadFromFile("report/license.key");  
-
-    Stimulsoft.Base.StiFontCollection.addOpentypeFontFile("report/BNazanin.ttf");
-    console.log("Font loaded");
-
-    var report = new Stimulsoft.Report.StiReport();
-    
-    report.loadFile("report/ArrivalNotice.mrt");
-    
+  
 // var dbcnn = report.dictionary.databases.getByName("cnn");
 // var config=sequelize.config
 // dbcnn.connectionString = `Data Source=${config.host};Initial Catalog=${config.database};Persist Security Info=True;User ID=${config.username};Password=${config.password}`;
@@ -30,26 +20,33 @@ try {
     })
     if (data.length>0) {
 
+    Stimulsoft.Base.StiLicense.loadFromFile("report/license.key");  
+
+    Stimulsoft.Base.StiFontCollection.addOpentypeFontFile("report/BNazanin.ttf");
+    console.log("Font loaded");
+
+    var report = new Stimulsoft.Report.StiReport();
+    
+    report.loadFile("report/ArrivalNotice.mrt");
+
     report.dictionary.variables.getByName("ID").valueObject = data[0].ID;
     report.dictionary.variables.getByName("User").valueObject = 4040;
 
     fileName=uuidv4()
 
     report.renderAsync(()=>{
-        // console.log("Report rendered. Pages count: ", report.renderedPages.count);
-
-        // // Saving rendered report to file
-
         var data = report.exportDocument(Stimulsoft.Report.StiExportFormat.Pdf);
 
         var buffer = new Buffer(data, "utf-8");
         var fs = require("fs");
         fs.writeFileSync(`report/temp/${fileName}.pdf`, buffer);
-    
+        console.log(1)
         res.download(`report/temp/${fileName}.pdf`)
 
     })
-    //res.redirect('/')
+}else{
+    req.flash("middlewareError" , "اعلامیه ورود صادر نشده است");
+    return res.redirect("/");
 }
 } catch (err) {
     console.log(err)
