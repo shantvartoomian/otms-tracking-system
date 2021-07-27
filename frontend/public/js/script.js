bd4ea3368b259e23384aba879201f3fb1957a9e3
shantvartoomian <shantvartoomian1@gmail.com>
2021-07-27 22:32:35 +0430

    ud

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -227,7 +227,7 @@ $(document).ready(function() {
   $(this).prop("disabled", true);
   // add spinner to button
   $(this).html(
-    `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`  );
+    `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Creating The File...`  );
 
   });
   });
